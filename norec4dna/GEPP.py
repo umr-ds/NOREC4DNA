fixed edgecases in decoded packet calculation for LT decoder + fixed some test cases + speedup
Signed-off-by: Michael Schwarz <schwarzk@mathematik.uni-marburg.de>

norec4dna/GEPP.py
@@ -194,13 +194,19 @@ class GEPP_intern:
 
     def isSolved(self) -> bool:
         all_true = set(x for x in range(self.m))
-        res = len(all_true) == len(set(x[0] for x in self.result_mapping))
+        solved_set = set(x[0] for x in self.result_mapping)
+        if -1 in solved_set:
+            solved_set.remove(-1)
+        res = len(all_true) == len(solved_set)
         if debug and not res:
-            print("Chunk(s) " + str(all_true - set(x[0] for x in self.result_mapping)) + " are missing. ")
+            print("Chunk(s) " + str(all_true - solved_set) + " are missing. ")
         return res
 
     def getSolvedCount(self) -> int:
-        return len(set(x[0] for x in self.result_mapping))
+        solved_set = set(x[0] for x in self.result_mapping)
+        if -1 in solved_set:
+            solved_set.remove(-1)
+        return len(solved_set)
 
     def get_common_packets(self, chunk_id_lst: typing.List[int], valid_chunks_lst: typing.List[int] = None,
                            multi_error_packet_mode=False) -> np.array:

norec4dna/RU10Packet.py
@@ -58,10 +58,12 @@ class RU10Packet(Packet):
     def set_used_packets(self, u_packets):
         self.used_packets = frozenset(u_packets)
         self.internal_hash = hash(self.used_packets)
-        tmp_lst = np.full(self.total_number_of_chunks, False, dtype=bool)
-        for x in self.used_packets:
-            if x < self.total_number_of_chunks:
-                tmp_lst[x] = True
+        tmp_lst = np.zeros(self.total_number_of_chunks, dtype=bool)
+        valid_indices = np.array(u_packets)[np.array(u_packets) < self.total_number_of_chunks]
+        tmp_lst[valid_indices] = True
+        #for x in self.used_packets:
+        #    if x < self.total_number_of_chunks:
+        #        tmp_lst[x] = True
         self.bool_arrayused_packets = tmp_lst
         self.update_degree()
         # [

norec4dna/helper/RU10Helper.py
@@ -26,19 +26,18 @@ def choose_packet_numbers(number_of_chunks: int, code_block_index: int, dist: Ra
     if d > l:
         d = l
     l = np.uint32(l)
-    indices: typing.List[int] = []
+    indices: typing.List[int] = [0] * d
     while b >= l:
         b = (b + a) % lprime
 
-    indices.append(b)
+    indices[0] = b
 
-    for _ in range(1, d):
+    for idx in range(1, d):
         b = (b + a) % lprime
         while b >= l:
             b = (b + a) % lprime
-        indices.append(b)
-    #indices.sort()
-    return indices
+        indices[idx] = b
+    return sorted(indices)
 
 
 @lru_cache(maxsize=None)