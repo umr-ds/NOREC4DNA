c2c7c668e96abd1623ecf41fb4a3fe7394358291
Peter Michael Schwarz <schwarzk@informatik.uni-marburg.de>
2025-10-13 17:45:47 +0200

    Add `store_parsed_packets` parameter to manage parsed packet storage during decoding

--- ConfigWorker.py
+++ ConfigWorker.py
@@ -17,14 +17,14 @@ class ConfigReadAndExecute:
         self.config.read(filename)
         self.coder = None
 
-    def execute(self, return_decoder: bool = False, skip_solve=False) -> typing.Optional[
+    def execute(self, return_decoder: bool = False, skip_solve=False, store_parsed_packets=False) -> typing.Optional[
         typing.List[typing.Union[str, Decoder]]]:
         decoders = []
         if len(self.config.sections()) == 0:
             print("Empty or missing config file. Does the config file exist?")
         for section in self.config.sections():
             print("Decoding {}".format(section))
-            decoder = self.__decode(section, self.config[section], return_decoder, skip_solve=skip_solve)
+            decoder = self.__decode(section, self.config[section], return_decoder, skip_solve=skip_solve, store_parsed_packets=store_parsed_packets)
             if return_decoder:
                 decoders.append(decoder)
         return decoders
@@ -41,7 +41,7 @@ class ConfigReadAndExecute:
                 print(f"[Warning] Config-entry '{cfg}' not known!")
 
     # @staticmethod
-    def __decode(self, filename, decode_conf, return_decoder=False, skip_solve=False):
+    def __decode(self, filename, decode_conf, return_decoder=False, skip_solve=False, store_parsed_packets=False):
         self.warn_unknown_items(decode_conf)
         algorithm = decode_conf.get("algorithm")
         number_of_chunks = decode_conf.getint("number_of_chunks", None)
@@ -104,7 +104,7 @@ class ConfigReadAndExecute:
                                 use_header_chunk=use_header_chunk, read_all=read_all_packets,
                                 distribution_cfg_str=distribution_cfg_str, return_decoder=return_decoder,
                                 checksum_len_str=checksum_len_str, skip_solve=skip_solve, xor_by_seed=xor_by_seed,
-                                id_spacing=id_spacing, mask_id=mask_id))
+                                id_spacing=id_spacing, mask_id=mask_id, store_parsed_packets=store_parsed_packets))
             except Exception as ex:
                 raise ex
         if len(folders) > 1:
